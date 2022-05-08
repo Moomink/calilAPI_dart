c3086d3614097557a218a4a5928b6e415f1d93e9
Moomink <hhd03295@gmail.com>
2022-05-08 23:11:03 +0900

    ADD: CalilLibrary Initilize

--- lib/src/APIs/library.dart
+++ lib/src/APIs/library.dart
@@ -1,4 +1,4 @@
-class CalilLibrary extends Calil {
+class CalilLibrary {
   final String systemid;
   final String systemname;
   final String libkey;
@@ -7,13 +7,33 @@ class CalilLibrary extends Calil {
   final String formal;
   final String url_pc;
   final String address;
+  final String pref;
   final String city;
   final String post;
   final String tel;
   final String geocode;
-  final String category;
+  final Category category;
   final String? image;
-  final String? distance;
+  final int? distance;
+
+  CalilLibrary({
+    required this.systemid,
+    required this.systemname,
+    required this.libkey,
+    required this.libid,
+    required this.short,
+    required this.formal,
+    required this.url_pc,
+    required this.address,
+    required this.pref,
+    required this.city,
+    required this.post,
+    required this.tel,
+    required this.geocode,
+    required this.category,
+    this.image,
+    this.distance,
+  });
 }
 
-enum category { small, medium, lagre, univ, special, bm }
+enum Category { small, medium, lagre, univ, special, bm }
