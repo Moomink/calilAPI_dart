2effb3a115157db7dc17ac0715cf03307a8b7280
Moomink <hhd03295@gmail.com>
2022-05-15 20:59:35 +0900

    ADD: example

--- example/calil_api_example.dart
+++ example/calil_api_example.dart
@@ -1,6 +1,10 @@
 import 'package:calil_api/calil_api.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+void main() async {
+  var calil = Calil("<Calil Api Key>");
+
+  List<CalilLibrary> library =
+      await calil.library(systemid: "Univ_Kisaraze_Ct");
+
+  print("lbrary Name: ${library[0].systemname}");
 }
