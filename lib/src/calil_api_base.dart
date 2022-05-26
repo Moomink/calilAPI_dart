3da5ba8846982d5736f1aea8d5b8cab3cb40ccb4
Moomink <hhd03295@gmail.com>
2022-05-26 22:19:13 +0900

    ADD: check API function

--- lib/src/calil_api_base.dart
+++ lib/src/calil_api_base.dart
@@ -1,4 +1,6 @@
 import 'APIs/library.dart';
+import 'APIs/check.dart';
+
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
@@ -56,5 +58,64 @@ class Calil {
     }
 
     return library;
+  }
+
+  Future<Map<String, List<CalilCheck>>> check(
+      List<String> isbn, List<String> systemid) async {
+    const String path = '/check';
+
+    Map<String, dynamic> query = {
+      "appkey": _appkey,
+      "isbn": isbn.join(","),
+      "systemid": systemid.join(","),
+      "format": "json",
+      "callback": "no"
+    };
+
+    Map<String, List<CalilCheck>> check = {};
+
+    Uri uri = Uri.https(apiDomain, path, query);
+
+    var client = http.Client();
+    Map data;
+    try {
+      http.Response res;
+      while (true) {
+        res = await client.get(uri);
+        if (jsonDecode(res.body)["continue"] == "1") {
+          query = {
+            "format": "json",
+            "callback": "no",
+            "session": jsonDecode(res.body)["session"]
+          };
+          uri = Uri.https(apiDomain, path, query);
+          await Future.delayed(Duration(seconds: 2));
+          continue;
+        } else {
+          break;
+        }
+      } //TODO check
+
+      data = jsonDecode(res.body);
+      // add library list
+      data["books"].forEach((id, libs) {
+        List<CalilCheck> librarys = [];
+        CalilCheck library;
+        libs.forEach((lib, values) {
+          library = CalilCheck(
+              status: values["status"],
+              reserveurl: values["reserveurl"],
+              libkey: values["libkey"]);
+          librarys.add(library);
+        });
+        check.addAll({id: librarys});
+      });
+    } catch (e) {
+      throw Exception(e);
+    } finally {
+      client.close();
+    }
+
+    return check;
   }
 }
