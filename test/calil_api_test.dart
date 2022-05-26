dcc0e0a00829a4e44251c03a4c57e97a1c71bba7
Moomink <hhd03295@gmail.com>
2022-05-27 01:13:35 +0900

    ADD: check API test

--- test/calil_api_test.dart
+++ test/calil_api_test.dart
@@ -23,4 +23,29 @@ void main() {
       expect(res[0].url_pc, "https://www.kisarazu.ac.jp/lib.html");
     });
   });
+
+  group("Collection Search", () {
+    test("API Response", () async {
+      var calil = Calil(apikey[0]);
+
+      Map<String, Map<String, CalilCheck>> res = await calil.check(
+          isbn: <String>["4062091542"],
+          systemid: <String>["Univ_Kisaraze_Ct", "Univ_Seiwa"]);
+
+      expect(res.containsKey("4062091542"), isTrue);
+      expect(res["4062091542"]!["Univ_Seiwa"]!.libkey["図書館"], "貸出可");
+      expect(
+          res["4062091542"]!["Univ_Seiwa"]!.reserveurl,
+          Uri(
+              scheme: 'https',
+              host: 'libopac3-c.nagaokaut.ac.jp',
+              path: '/opac/opac_details/',
+              queryParameters: {
+                'lang': 0,
+                'amode': 11,
+                'bibid': 1013026374,
+                'kscode': 015
+              }));
+    });
+  });
 }
