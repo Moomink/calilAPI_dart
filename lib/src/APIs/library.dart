f238238180176384087ab769160fc5866575b56d
Moomink <hhd03295@gmail.com>
2022-07-30 07:58:11 +0900

    ADD: add library function

--- lib/src/APIs/library.dart
+++ lib/src/APIs/library.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const String apiDomain = "api.calil.jp";
+
 class CalilLibrary {
   final String systemid;
   final String systemname;
@@ -84,5 +89,51 @@ class CalilLibrary {
       this.faid});
 }
 
-//TODO add enum
-//enum Category { SMALL, MEDIUM, LAGRE, UNIV, SPECIAL, BM }
+Future<List<CalilLibrary>> searchLibrary(
+    {String pref = "",
+    String city = "",
+    String systemid = "",
+    String geocode = "",
+    int limit = 5}) async {
+  const String path = '/library';
+
+  Map<String, dynamic> query = {
+    //"appkey": _appkey,
+    "pref": pref,
+    "city": city,
+    "systemid": systemid,
+    "geocode": geocode,
+    "limit": limit.toString(),
+    "format": "json",
+    "callback": "no"
+  };
+
+  final Uri uri = Uri.https(apiDomain, path, query);
+
+  var client = http.Client();
+
+  List<CalilLibrary> library = [];
+  List<Map<String, dynamic>> getData = [];
+
+  try {
+    http.Response res;
+    res = await client.get(uri);
+
+    for (var value in jsonDecode(res.body)) {
+      getData.add(value);
+    }
+  } catch (e) {
+    throw Exception(e);
+  } finally {
+    client.close();
+
+    if (getData.isEmpty) {
+    } else {
+      for (var value in getData) {
+        library.add(CalilLibrary.fromJson(value));
+      }
+    }
+  }
+
+  return library;
+}
